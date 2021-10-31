afb28d3bb83afa70b59bee2d8e5f2f254659034e
Mike Tickle <mtickle@gmail.com>
2021-10-31 10:38:44 -0400

    fixed bootstrap and jQuery

--- index.php
+++ index.php
@@ -12,14 +12,14 @@ error_reporting(E_ALL);
 <html lang="en">
 <head>
   <meta charset="UTF-8">
-  <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-  <title>Simple table listing using Postgres database</title>
+  <script type="text/javascript" src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <title>Simple table listing using Postgres database</title>
 </head>
 <body>
   <div class="container">
-    <div class="col-sm-6" style="padding-top:50px;">
+    <div class="col-12" style="padding-top:50px;">
         <div class="well">
             <h2>Simple table listing using PHP and Postgres database</h2>
         </div>
