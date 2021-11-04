80d978e3372f9046cbc6deffe4c8235b0802a6f6
Mike Tickle <mtickle@gmail.com>
2021-11-04 11:40:55 -0400

    help me

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-    include("response.php");
+    include("incident.php");
     $newObj = new Incident();
     $emps = $newObj->getIncidents();
 ?>
@@ -7,13 +7,18 @@
 <html lang="en">
 <head>
   <meta charset="UTF-8">
-  <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  <!-- <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script> -->
   <title>Simple table listing using Postgres database</title>
 </head>
 <body>
   <div class="container">
+
+
+  <canvas id="myChart" width="400" height="300"></canvas>
+
+
     <div class="col-sm-6" style="padding-top:50px;">
         <div class="well">
             <h2>Simple table listing using PHP and Postgres database</h2>
@@ -42,5 +47,16 @@
     </div>
   </div>
 </div>
+
+
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.23.0/moment.min.js"></script>
+
+<?php
+      include("chart.php");  
+?>
+
 </body>
 </html>
