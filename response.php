e54b026d477c038bd21f4f88cfaba748848b1bd0
Mike Tickle <mtickle@gmail.com>
2021-11-04 10:03:03 -0400

    checkpoint

--- response.php
+++ response.php
@@ -11,7 +11,15 @@ class Incident {
 	}
 	
 	public function getIncidents() {
-		$sql = "SELECT * FROM public.incident";
+		$sql = "SELECT * FROM get_incidents();";
+		//$sql = "SELECT * FROM public.incident";
+                //$sql = "select * from public.incident where"
+                //$sql .= " (EXTRACT(YEAR FROM incident.""ReportedDate"") = '2021'"
+                //$sql .= " AND EXTRACT(MONTH FROM incident.""ReportedDate"") = '10')"
+                //$sql .= " ORDER BY incident.""ReportedDate"" DESC"
+
+	
+
 		$queryRecords = pg_query($this->conn, $sql) or die("error to fetch incidents data");
 		$data = pg_fetch_all($queryRecords);
 		return $data;
