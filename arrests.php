c84b47fd92f8b93934c73e7056c7fac23a49de7c
Mike Tickle <mtickle@gmail.com>
2021-11-07 08:47:22 -0500

    maybe

--- arrests.php
+++ arrests.php
@@ -6,16 +6,17 @@
     include 'includes/header.php';
 ?>
 
-
-  <div class="container">
-
-
-<div class="chart-container" style="height:400px; width:800px">
-    <canvas id="myChart"></canvas>
-</div>
+  <div class="container-lg pb-3">
+    <div class="d-grid gap-3" style="grid-template-columns: 1fr 1fr;">
+      <div class="bg-light border rounded-3">
+            <div class="chart-container" style="height:400px; width:800px">
+                <canvas id="myChart"></canvas>
+            </div>      
+      </div>
       
-
-  <table id="charge_grid" class="table" width="100%" cellspacing="0">
+      <div class="bg-light border rounded-3">
+      
+      <table id="charge_grid" class="table" width="100%" cellspacing="0">
             <thead>
                 <tr>
                     <th>Charge</th>
@@ -32,12 +33,23 @@
             </tbody>
         </table>
 
+      
+      </div>
+    </div>
+  </div>
+
+
+
+
+      
+
+  
       </div>
 
 
 
 
 <?php
-      include("charts/arrests_chart.php");  
+      include("assets/arrests_chart.php");  
       include("includes/footer.php")
 ?>
