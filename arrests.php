f61a48b6c222b1e788de8f26abce23468d6b18cd
Mike Tickle <mtickle@gmail.com>
2021-11-07 08:58:04 -0500

    yikes

--- arrests.php
+++ arrests.php
@@ -1,55 +1,21 @@
 
-<?php
-    include("classes/incident.php");
-    $newObj = new Incident();
-    $charges = $newObj->getTopCharges();
-    include 'includes/header.php';
-?>
+<?php include 'includes/header.php'; ?>
 
   <div class="container-lg pb-3">
     <div class="d-grid gap-3" style="grid-template-columns: 1fr 1fr;">
+      
+        <div class="bg-light border rounded-3">
+            <?php include("includes/chart_container.php"); ?>
+        </div>
+      
       <div class="bg-light border rounded-3">
-            <div class="chart-container" >
-                <canvas id="myChart"></canvas>
-            </div>      
+        <?php include("tables/arrests_table.php"); ?>     
       </div>
-      
-      <div class="bg-light border rounded-3">
-      
-      <table id="charge_grid" class="table" width="100%" cellspacing="0">
-            <thead>
-                <tr>
-                    <th>Charge</th>
-                    <th>Count</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php foreach($charges as $key => $charge) :?>
-                <tr>
-                    <td><?php echo $charge['charge'] ?></td>
-                    <td><?php echo $charge['charge_count'] ?></td>
-                </tr>
-            <?php endforeach;?>
-            </tbody>
-        </table>
 
-      
-      </div>
     </div>
   </div>
 
-
-
-
-      
-
-  
-      </div>
-
-
-
-
 <?php
-      include("assets/arrests_chart.php");  
+      include("charts/arrests_chart.php");  
       include("includes/footer.php")
 ?>
--- incidents.php
+++ incidents.php
@@ -1,43 +1,21 @@
 
-<?php
-   include("classes/incident.php");
-    $newObj = new Incident();
-    $incidents = $newObj->getIncidents();
-    include 'includes/header.php';
-?>
+<?php include 'includes/header.php'; ?>
 
-<div class="container">
-
-        <div class="chart-container" style="height:400px; width:800px">
-            <canvas id="myChart"></canvas>
+<div class="container-lg pb-3">
+    <div class="d-grid gap-3" style="grid-template-columns: 1fr 1fr;">
+      
+        <div class="bg-light border rounded-3">
+            <?php include("includes/chart_container.php"); ?>
         </div>
-     
-
-  	<table id="incidents_grid" class="table" width="100%" cellspacing="0">
-            <thead>
-                <tr>
-                    <th>Date</th>
-                    <th>Description</th>
-                    <th>Address</th>
-                    <th>District</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php foreach($incidents as $key => $incident) :?>
-                <tr>
-                    <td><?php echo $incident['reported_date'] ?></td>
-                    <td><?php echo $incident['crime_description'] ?></td>
-                    <td><?php echo $incident['reported_block_address'] ?></td>
-                    <td><?php echo $incident['district'] ?></td>
-                </tr>
-            <?php endforeach;?>
-            </tbody>
-        </table>
-   
+      
+      <div class="bg-light border rounded-3">
+            <?php include("tables/incidents_table.php"); ?>     
       </div>
 
+    </div>
+  </div>
 
 <?php
-      include("assets/incidents_chart.php");
+      include("charts/incidents_chart.php");
       include("includes/footer.php")  
 ?>
