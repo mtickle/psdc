60e63cef9048e62ec63d33c5e11c21dd4deb5f88
Mike Tickle <mtickle@gmail.com>
2021-11-08 06:22:15 -0500

    added homepage buttons

--- index.php
+++ index.php
@@ -5,19 +5,38 @@
 
   <div class="container-lg pb-3">
     <div class="d-grid gap-3" style="grid-template-columns: 1fr 1fr;">
-      <div class="bg-light border rounded-3">
-      
-      Raleigh Police Department incidents reported yesterday using the National Incident Based Reporting System (NIBRS).
+      <div>
 
-      <a href="incidents.php" class="btn btn-secondary btn-sm btn-block" role="button" aria-disabled="true">View Incidents</a>
-      
+
+<div class="card">
+  <div class="card-header">
+    Raleigh Police Department
+  </div>
+  <div class="card-body">
+    <h5 class="card-title">Incident Data</h5>
+    <p class="card-text">Raleigh Police Department incidents reported yesterday using the National Incident Based Reporting System (NIBRS).
+</p>
+    <a href="incidents.php" class="btn btn-primary">View Incidents</a>
+  </div>
+</div>
 
 
       </div>
       
-      <div class="bg-light border rounded-3">
-      <a href="arrests.php" class="btn btn-secondary btn-sm btn-block" role="button" aria-disabled="true">View Arrests</a>
-      </div>
+      <div>
+<div class="card">
+  <div class="card-header">
+    Wake County CCBI
+  </div>
+  <div class="card-body">
+    <h5 class="card-title">Arrest Data</h5>
+    <p class="card-text">CCBI anonymized arrest data for the past 30 days.</p>
+    <a href="arrests.php" class="btn btn-primary">View Arrests</a>
+  </div>
+</div>
+
+
+</div>
     </div>
   </div>
 
