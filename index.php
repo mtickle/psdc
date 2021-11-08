c2a66d3fc8aa636ee8bde4b81c4a26a042d5e592
Mike Tickle <mtickle@gmail.com>
2021-11-08 06:11:01 -0500

    Update index.php

--- index.php
+++ index.php
@@ -6,10 +6,14 @@
   <div class="container-lg pb-3">
     <div class="d-grid gap-3" style="grid-template-columns: 1fr 2fr;">
       <div class="bg-light border rounded-3">
-        <br><br><br><br><br><br><br><br><br><br>
+      
+      <a href="incidents.php" class="btn btn-primary btn-lg" role="button" aria-disabled="true">View Incidents</a>
+      
+
+
       </div>
       <div class="bg-light border rounded-3">
-        <br><br><br><br><br><br><br><br><br><br>
+      <a href="arrests.php" class="btn btn-primary btn-lg" role="button" aria-disabled="true">View Arrests</a>
       </div>
     </div>
   </div>
