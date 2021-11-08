fa4747ac79e4c71caf566d51244c1c28814c2235
Mike Tickle <mtickle@gmail.com>
2021-11-08 06:16:35 -0500

    Update index.php

--- index.php
+++ index.php
@@ -7,6 +7,8 @@
     <div class="d-grid gap-3" style="grid-template-columns: 1fr 1fr;">
       <div class="bg-light border rounded-3">
       
+      Raleigh Police Department incidents reported yesterday using the National Incident Based Reporting System (NIBRS).
+
       <a href="incidents.php" class="btn btn-secondary btn-sm btn-block" role="button" aria-disabled="true">View Incidents</a>
       
 
