759a23c459366afd3f286f5548a8fc7f725a8866
Mike Tickle <mtickle@gmail.com>
2021-11-08 14:10:56 -0500

    Update arrests.php

--- arrests.php
+++ arrests.php
@@ -4,11 +4,30 @@
 <div class="container">
     <div class="row">
         <div class="col-sm-6">
-            <?php include("includes/chart_container.php"); ?>
+
+            <div class="card">
+                <div class="card-header">
+                    Analysis
+                </div>
+                <div class="card-body">
+                    <?php include("includes/chart_container.php"); ?>
+                </div>
+            </div>
+
+
         </div>
         <div class="col-sm-6">
-            <?php include("tables/arrests_table.php"); ?>
-        </div>
+
+
+            <div class="card">
+                <div class="card-header">
+                    Data
+                </div>
+                <div class="card-body">
+                    <?php include("tables/arrests_table.php"); ?>
+                </div>
+            </div>
+      </div>
     </div>
 </div>
 
