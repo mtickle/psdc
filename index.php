cb85ea15b8cfdf77b5085db96f1536f15fa1e0c7
Mike Tickle <mtickle@gmail.com>
2021-11-08 06:13:03 -0500

    Added buttons

--- index.php
+++ index.php
@@ -6,27 +6,15 @@
   <div class="container-lg pb-3">
     <div class="d-grid gap-3" style="grid-template-columns: 1fr 1fr;">
       <div class="bg-light border rounded-3">
-<<<<<<< HEAD
       
       <a href="incidents.php" class="btn btn-primary btn-lg" role="button" aria-disabled="true">View Incidents</a>
       
 
 
-=======
-
-        <br><br><br><br><br><br><br><br><br><br>
-      
->>>>>>> 0316ec4343eadc6e796b89c927277bc7a843ea65
       </div>
       
       <div class="bg-light border rounded-3">
-<<<<<<< HEAD
       <a href="arrests.php" class="btn btn-primary btn-lg" role="button" aria-disabled="true">View Arrests</a>
-=======
-      
-      <br><br><br><br><br><br><br><br><br><br>
-      
->>>>>>> 0316ec4343eadc6e796b89c927277bc7a843ea65
       </div>
     </div>
   </div>
