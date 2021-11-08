16db865333edad1c830867832b9b73e691dc3e80
Mike Tickle <mtickle@gmail.com>
2021-11-08 06:32:28 -0500

    whooop

---
+++ about.php
@@ -0,0 +1,20 @@
+
+<?php include 'includes/header.php'; ?>
+
+  <div class="container-lg pb-3">
+    <div class="d-grid gap-3" style="grid-template-columns: 1fr 1fr;">
+      
+        <div class="bg-light border rounded-3">
+            
+        </div>
+      
+      <div class="bg-light border rounded-3">
+            
+      </div>
+
+    </div>
+  </div>
+
+<?php
+      include("includes/footer.php")
+?>
