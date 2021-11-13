47766839ca037e553ef980c2e59bb2efa7cfecd1
Mike Tickle <mtickle@gmail.com>
2021-11-13 07:43:44 -0500

    checky checky

--- includes/header.php
+++ includes/header.php
@@ -35,6 +35,13 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
+        integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
+        crossorigin="" />
+    <!-- Make sure you put this AFTER Leaflet's CSS -->
+    <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"
+        integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA=="
+        crossorigin=""></script>
 
 </head>
 
@@ -52,11 +59,16 @@
 
                 <div class="collapse navbar-collapse" id="navbarsExample07">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li class="nav-item"><a href="index.php" class="nav-link <?php echo $index_active;?>">Home</a></li>
-                        <li class="nav-item"><a href="incidents.php" class="nav-link <?php echo $incidents_active;?>">Incidents</a></li>
-                        <li class="nav-item"><a href="arrests.php" class="nav-link <?php echo $arrests_active;?>">Arrests</a></li>
-                                <li class="nav-item"><a href="calls.php" class="nav-link <?php echo $calls_active;?>">Calls</a></li>
-                                <li class="nav-item"><a href="about.php" class="nav-link <?php echo $about_active;?>">About</a></li>
+                        <li class="nav-item"><a href="index.php" class="nav-link <?php echo $index_active;?>">Home</a>
+                        </li>
+                        <li class="nav-item"><a href="incidents.php"
+                                class="nav-link <?php echo $incidents_active;?>">Incidents</a></li>
+                        <li class="nav-item"><a href="arrests.php"
+                                class="nav-link <?php echo $arrests_active;?>">Arrests</a></li>
+                        <li class="nav-item"><a href="calls.php" class="nav-link <?php echo $calls_active;?>">Calls</a>
+                        </li>
+                        <li class="nav-item"><a href="about.php" class="nav-link <?php echo $about_active;?>">About</a>
+                        </li>
                     </ul>
                     <form>
                         <input class="form-control" type="text" placeholder="Search" aria-label="Search">
---
+++ maps/calls_map.php
@@ -0,0 +1,33 @@
+
+
+
+
+
+
+<div id="map"></div>
+
+<script>
+
+ var mymap = L.map('map').setView([35.7796, -78.6382], 10);
+
+ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+   maxZoom: 20,
+   attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
+     'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+   id: 'mapbox/streets-v11',
+   tileSize: 512,
+   zoomOffset: -1
+ }).addTo(mymap);
+
+
+ L.marker([35.748218, -78.875430]).addTo(mymap)
+   .bindPopup("Road Hazard").openPopup();
+
+   L.marker([35.816789, -78.594963]).addTo(mymap)
+   .bindPopup("MVC Injuries 29B1").openPopup();  
+
+ //var popup = L.popup();
+
+ 
+
+</script>
