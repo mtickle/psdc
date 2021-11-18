9a47635b7958ebb45cc8b26a5e7336a6da4cccbf
Mike Tickle <mtickle@gmail.com>
2021-11-18 06:30:08 -0500

    Update recent_calls_cards.php

--- tables/recent_calls_cards.php
+++ tables/recent_calls_cards.php
@@ -4,6 +4,13 @@ $objIncident = new Incident();
 $calls = $objIncident->getRecentCalls();
 ?>
 
+<style>
+#map { 
+    height: 380px; 
+    width: 900px;
+  }
+
+</style>
 
 <?php foreach($calls as $key => $call) :?>
     <div class="card">
@@ -11,7 +18,9 @@ $calls = $objIncident->getRecentCalls();
             <?php echo $call['agency'] ?>
         </div>
         <div class="card-body">
-            <h5 class="card-title"><?php echo $call['incident'] ?></h5>
+        
+        <div id="map-<?php echo $call['id'] ?>"></div>
+        <h5 class="card-title"><?php echo $call['incident'] ?></h5>
 	    <h6 class="card-subtitle mb-2 text-muted"><?php echo $call['location'] ?></h6>
             <p>
             <?php echo $call['call_date'] ?> <?php echo $call['call_time'] ?>
@@ -19,5 +28,21 @@ $calls = $objIncident->getRecentCalls();
         </div>
     </div>
 <br />
-    <?php endforeach;?>
+
+<script>
+
+ var mymap = L.map('map-<?php echo $call['id'] ?>').setView([35.7796, -78.6382], 10);
+
+ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+   maxZoom: 20,
+   attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
+     'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+   id: 'mapbox/streets-v11',
+   tileSize: 512,
+   zoomOffset: -1
+ }).addTo(mymap);
+L.marker([<?php echo $call['latitude'] ?>, <?php echo $call['longitude'] ?>]).addTo(mymap).bindPopup("<?php echo $call['incident'] ?>").openPopup();
+</script>
+
+<?php endforeach;?>
 
