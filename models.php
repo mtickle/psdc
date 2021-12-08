80482e802dbf10267c125567fac222fb736fde1f
Mike Tickle <mtickle@gmail.com>
2021-12-08 13:40:00 -0500

    Create models.php

---
+++ models.php
@@ -0,0 +1,22 @@
+<?php include 'includes/header.php'; ?>
+
+
+<div class="container">
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="card">
+                    <div class="card-header">
+                        Data
+                    </div>
+                    <div class="card-body">
+                        <?php include("tables/models_table.php"); ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</div>
+<?php
+      include("includes/footer.php")
+?>
