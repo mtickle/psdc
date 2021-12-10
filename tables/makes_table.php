a1f308aeb9ba5868f3ad0d57333256aa0a32ec26
Mike Tickle <mtickle@gmail.com>
2021-12-10 06:49:31 -0500

    Update makes_table.php

--- tables/makes_table.php
+++ tables/makes_table.php
@@ -8,12 +8,16 @@ $makes = $objIncident->getMakes();
     <table id="charge_grid" class="table" width="100%" cellspacing="0">
         <thead>
             <tr>
+                <th>Make ID</th>
                 <th>Make Name</th>
+                <th>Count</th>
             </tr>
         </thead>
         <tbody>
             <?php foreach($makes as $key => $make) :?>
             <tr>
+                <td><?php echo $make['make_id'] ?></td>
+                <td><?php echo $make['make_name'] ?></td>
                 <td><a href="models.php?make_id=<?php echo $make['make_id'] ?>"><?php echo $make['make_name'] ?></a></td>
         	</tr>
         <?php endforeach;?>
