50965559f6e8d3efc1fad713f0d766d249e81280
Mike Tickle <mtickle@gmail.com>
2021-12-12 08:04:16 -0500

    Update about.php

--- about.php
+++ about.php
@@ -1,22 +1,55 @@
-
 <?php include 'includes/header.php'; ?>
 
-  <div class="container">
-    
+<div class="container">
 
-  
-<h5>Built with ...</h5>
-<ul>
-    <li>Leaflet.js</li>
-    <li>Chart.js</li>
-    <li>Go</li>
-    <li>PHP</li>
-    <li>PostgresSQL</li>
-    <li>Apache</li>
-</ul>
+    <div class="row">
+
+        <div class="col-sm-6">
+            <div class="card">
+                <div class="card-header">
+                    Technology
+                </div>
+                <div class="card-body">
+                    <div class="chart-container">
+                        <ul>
+                            <li>Leaflet.js</li>
+                            <li>Chart.js</li>
+                            <li>Go</li>
+                            <li>PHP</li>
+                            <li>PostgresSQL</li>
+                            <li>Apache</li>
+                        </ul>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-6">
+            <div class="card">
+                <div class="card-header">
+                    Public Data Sources
+                </div>
+                <div class="card-body">
+                    <div class="chart-container">
+
+                    <ul>
+                      <li><a href="http://incidents.rwecc.com/" >Raleigh - Wake County 911 Incident Data</a></li>
+                      <li><a href="https://data-ral.opendata.arcgis.com/datasets/ral::persons-involved-in-crashes/about">Persons Involved in Crashes</a></li>
+                      <li><a href="https://data-ral.opendata.arcgis.com/datasets/ral::daily-raleigh-police-incidents/about">Daily Raleigh Police Incidents</a></li>
+                      <li><a href="https://dwslivescan.co.wake.nc.us/mug/Disclaimer.aspx">CCBI Arrest Portal</a></li>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
 
-  </div>
+    <h5>Built with ...</h5>
+
+
+
+</div>
 
 <?php
       include("includes/footer.php")
