a18cd368368b95a67d0860724bb6bdd238b23093
Mike Tickle <mtickle@gmail.com>
2021-12-12 07:37:43 -0500

    restructuring

--- index.php
+++ index.php
@@ -1,88 +1,85 @@
-
-  
 <?php include 'includes/header.php'; ?>
+
 
 <div class="container">
 
-
-    <div class="card">
-        <div class="card-header">
-          Incidents
-        </div>
-        <div class="card-body">
-        <div class="row">
-            <div class="col-sm-6">
-               <h5 class="card-title">Incidents by Type</h5>
-               <div class="chart-container" >
-                    <canvas id="IncidentAnalysis"></canvas>
-                 </div>  
-            </div>
-            <div class="col-sm-6">
-                <h5 class="card-title">Incidents by District</h5>
-                <div class="chart-container" >
-                    <canvas id="IncidentsByDistrict"></canvas>
-                </div> 
-            </div>
-            </div>
-        </div>  
-    </div>
-
-    <p></p>
-    
-    <div class="card">
-        <div class="card-header">
-          Arrests
-        </div>
-        <div class="card-body">
-        <div class="row">
-            <div class="col-sm-6">
-               <h5 class="card-title">Arrests by Charge</h5>
-               <div class="chart-container">
-               <canvas id="ArrestsByChargeChart"></canvas>
-           </div>
-            </div>
-            <div class="col-sm-6">
-                <h5 class="card-title">Arrests by Gender</h5>
-                <div class="chart-container">
-                    <canvas id="ArrestsByGenderChart"></canvas>
+    <div class="row">
+        <div class="col-sm-6">
+            <div class="card">
+                <div class="card-header">
+                    RPD Incidents
+                </div>
+                <div class="card-body">
+                    <div class="chart-container">
+                        container
+                    </div>
                 </div>
             </div>
-            </div>
-        </div>  
-    </div>
-
-    <p></p>
-    
-    <div class="card">
-        <div class="card-header">
-          Calls for Assistance
         </div>
-        <div class="card-body">
-        <div class="row">
-            <div class="col-sm-6">
-               <h5 class="card-title">Calls by Type</h5>
-               <div class="chart-container">
-                <canvas id="CallsByTypeChart"></canvas>
-            </div>
-            </div>
-            <div class="col-sm-6">
-                <h5 class="card-title">Calls by Agency</h5>
-                <div class="chart-container">
-                    <canvas id="CallsByAgencyChart"></canvas>
+        <div class="col-sm-6">
+            <div class="card">
+                <div class="card-header">
+                    CCBI Arrest Inprocessing
+                </div>
+                <div class="card-body">
+                    <div class="chart-container">
+                        container
+                    </div>
                 </div>
             </div>
+        </div>
+    </div>
+
+
+
+    <div class="row">
+        <div class="col-sm-6">
+            <div class="card">
+                <div class="card-header">
+                    911 Center Calls
+                </div>
+                <div class="card-body">
+                    <div class="chart-container">
+                        container
+                    </div>
+                </div>
             </div>
-        </div>  
+        </div>
+        <div class="col-sm-6">
+            <div class="card">
+                <div class="card-header">
+                    MVC Persons
+                </div>
+                <div class="card-body">
+                    <div class="chart-container">
+                        container
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-sm-6">
+            <div class="card">
+                <div class="card-header">
+                    Vehicle Make/Model Metadata
+                </div>
+                <div class="card-body">
+                    <div class="chart-container">
+                        container
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-6">
+          
+        </div>
     </div>
 
 </div>
-  
 <?php
       include("charts/arrests_chart.php"); 
       include("charts/arrests_by_gender_chart.php");  
-      include("charts/incidents_chart.php");
-      include("charts/incidents_by_district_chart.php");
-      include("charts/call_types_chart.php");
-      include("charts/call_agencies_chart.php");
       include("includes/footer.php")
 ?>
