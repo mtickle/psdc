58fabb855ad9a796da20c8a7223cad027d75d0e0
Mike Tickle <mtickle@gmail.com>
2022-04-07 13:14:23 -0400

    Create calls.php

---
+++ calls.php
@@ -0,0 +1,34 @@
+<?php include 'includes/header.php';?>
+
+
+<div class="container">
+<div class="row">
+        <div class="col-sm-9">
+        <div class="card">
+                <div class="card-header">
+                    Calls Map
+                </div>
+                <div class="card-body">
+                    <?php include("maps/calls_map.php"); ?>
+                </div>
+            </div>
+	<br/>
+        </div>
+
+        <div class="col-sm-3">
+
+        </div>
+    </div>    
+    <div class="row">
+        <div class="col-sm-9">
+             <?php include("tables/recent_calls_cards.php"); ?>
+        </div>
+
+        <div class="col-sm-3">
+
+        </div>
+    </div>
+</div>
+
+
+<?php include 'includes/footer.php';?>
