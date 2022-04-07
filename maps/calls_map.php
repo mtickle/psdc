f520e76a9136dbec6d22c13896eeacc79d59315a
Mike Tickle <mtickle@gmail.com>
2022-04-07 13:26:38 -0400

    Update calls_map.php

--- maps/calls_map.php
+++ maps/calls_map.php
@@ -30,7 +30,7 @@ $calls = $objIncident->getRecentCalls();
 
 
  <?php foreach($calls as $key => $call) :?>
-    L.marker([<?php echo $call['latitude'] ?>, <?php echo $call['longitude'] ?>]).addTo(mymap).bindPopup("<?php echo $call['incident'] ?><?php echo $call['latitude'] ?>").openPopup();
+    L.marker([<?php echo $call['latitude'] ?>, <?php echo $call['longitude'] ?>]).addTo(mymap).bindPopup("<?php echo $call['agency'] ?><br /><?php echo $call['incident'] ?>").openPopup();
   <?php endforeach;?>
 
 </script>
