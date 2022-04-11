9c680d207d53862cf2115abaf0e7be1e867c0f8c
Mike Tickle <mtickle@gmail.com>
2022-04-11 11:32:51 -0400

    Update calls.php

--- calls.php
+++ calls.php
@@ -3,7 +3,7 @@
 
 <div class="container">
 <div class="row">
-        <div class="col-sm-9">
+        <div class="col-lg-12">
         <div class="card">
                 <div class="card-header">
                     Calls Map
