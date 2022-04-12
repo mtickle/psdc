33f76eccf2f980554d5345df8db7f56a850751db
Mike Tickle <mtickle@gmail.com>
2022-04-12 13:47:30 -0400

    Update vin.php

--- vin.php
+++ vin.php
@@ -2,28 +2,29 @@
 
 
 <div class="container">
-        <div class="row">
-            <div class="col-sm-12">
-                <div class="card">
-                    <div class="card-header">
-                        Data
-                    </div>
-                    <div class="card-body">
-                    
-                    <form action="vin_decode.php" method="get">
-  <div class="form-group">
-    <label for="exampleInputEmail1">VIN</label>
-    <input type="text" class="form-control" id="vin" aria-describedby="vin" placeholder="1C4RJFBM2GC306164">
-  </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
-</form>
+    <div class="row">
+        <div class="col-sm-12">
+            <div class="card">
+                <div class="card-header">
+                    Data
+                </div>
+                <div class="card-body">
 
-                    </div>
+                    <form action="vin_decode.php" method="get">
+                        <div class="form-group">
+                            <label for="exampleInputEmail1">VIN</label>
+                            <input type="text" class="form-control" id="vin" aria-describedby="vin"
+                                placeholder="1C4RJFBM2GC306164">
+                        </div>
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </form>
+
                 </div>
             </div>
         </div>
-
     </div>
+
+</div>
 </div>
 <?php
       include("includes/footer.php")
