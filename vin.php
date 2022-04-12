e3359dc47f541cd332a234378b807a44daa6e130
Mike Tickle <mtickle@gmail.com>
2022-04-12 13:37:54 -0400

    Create vin.php

---
+++ vin.php
@@ -0,0 +1,30 @@
+<?php include 'includes/header.php'; ?>
+
+
+<div class="container">
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="card">
+                    <div class="card-header">
+                        Data
+                    </div>
+                    <div class="card-body">
+                    
+                    <form>
+  <div class="form-group">
+    <label for="exampleInputEmail1">VIN</label>
+    <input type="email" class="form-control" id="vin" aria-describedby="vin" placeholder="1C4RJFBM2GC306164">
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</div>
+<?php
+      include("includes/footer.php")
+?>
