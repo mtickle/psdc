c66b3a1f3544d2068082750cbba2bcc7568e5364
Mike Tickle <mtickle@gmail.com>
2022-04-13 09:32:13 -0400

    Update vin_decode.php

--- vin_decode.php
+++ vin_decode.php
@@ -4,10 +4,14 @@
 
 <?php
 
-in_vin =  $_POST["vin"]; 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+$in_vin =  $_POST["vin"]; 
 
 
-echo in_vin;
+echo $in_vin;
 
 //$response = file_get_contents('https://vpic.nhtsa.dot.gov/api/vehicles/decodevinvalues/' + 1C4RJFBM2GC306164 ' ?format=xml');
 
