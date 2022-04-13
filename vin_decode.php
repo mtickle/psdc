ed4e20d704f49bb68569e78851e6f3b2233d92a0
Mike Tickle <mtickle@gmail.com>
2022-04-13 11:16:54 -0400

    updated vin decode

--- vin_decode.php
+++ vin_decode.php
@@ -7,16 +7,32 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 $in_vin =  $_POST["vin"]; 
-$format = "json"
+$format = "json";
 $url = 'https://vpic.nhtsa.dot.gov/api/vehicles/decodevinvalues/' . $in_vin . '?format=' . $format;
-//$response = file_get_contents();
-echo $url;
+$json = file_get_contents($url);
 
-//$json = '{"foo-bar": 12345}';
-//$obj = json_decode($json);
-//print $obj->{'foo-bar'}; // 12345
+$decode = json_decode($json, true);
 
+$results = array();
 
+foreach($decode['Results'] as $chunk) {
+
+$BodyClass = $chunk['BodyClass'];
+$VehicleType = $chunk['VehicleType'];
+$FuelTypePrimary = $chunk['FuelTypePrimary'];
+$Make = $chunk['Make'];
+$Model = $chunk['Model'];
+$ModelYear = $chunk['ModelYear'];
+$VIN = $chunk['VIN'];
+}
+
+echo $BodyClass .'<br/>';
+echo $VehicleType .'<br/>';
+echo $FuelTypePrimary . '<br />';
+echo $Make . '<br />';
+echo $Model . '<br />';
+echo $ModelYear . '<br />';
+echo $VIN . '<br />';
 
 ?>
 
