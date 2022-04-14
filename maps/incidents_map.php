b40a8e4ec6ffcb8b50264b85ac0ffbfc2f0e2995
Mike Tickle <mtickle@gmail.com>
2022-04-14 10:33:51 -0400

    Update incidents_map.php

--- maps/incidents_map.php
+++ maps/incidents_map.php
@@ -17,7 +17,7 @@ $items = $objIncident->getRecentIncidents();
 
 <script>
 
- var mymap = L.map('map').setView([35.7796, -78.6382], 13);
+ var mymap = L.map('map').setView([35.7796, -78.6382], 12);
 
  L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
    maxZoom: 20,
