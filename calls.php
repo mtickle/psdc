fd7638e363fdd2468466e3bb46de1d330c15b4ca
Mike Tickle <mtickle@gmail.com>
2022-04-14 08:58:04 -0400

    updating

--- calls.php
+++ calls.php
@@ -6,7 +6,7 @@
         <div class="col-lg-12">
             <div class="card">
                 <div class="card-header">
-                    Calls Map
+                    Recent Calls Map
                 </div>
                 <div class="card-body">
                     <?php include("maps/calls_map.php"); ?>
@@ -14,12 +14,42 @@
             </div>
         </div>
     </div>
-        <br />
-        <div class="row">
-            <div class="col-lg-12">
-                <?php include("tables/recent_calls_cards.php"); ?>
+    <br />
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="card">
+                <div class="card-header">
+                    Calls for Assistance
+                </div>
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <h5 class="card-title">Calls by Type</h5>
+                            <div class="chart-container">
+                                <canvas id="CallsByTypeChart"></canvas>
+                            </div>
+                        </div>
+                        <div class="col-sm-6">
+                            <h5 class="card-title">Calls by Agency</h5>
+                            <div class="chart-container">
+                                <canvas id="CallsByAgencyChart"></canvas>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
+    </div>
+    <br />
+    <div class="row">
+        <div class="col-lg-12">
+            <?php include("tables/recent_calls_cards.php"); ?>
+        </div>
+    </div>
 
 
-    <?php include 'includes/footer.php';?>
+    <?php 
+        include("charts/call_types_chart.php");
+        include("charts/call_agencies_chart.php");
+        include 'includes/footer.php';
+    ?>
