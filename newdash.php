bc51598eed0d4549f03ee6a92dc9a5c73b24df8b
Mike Tickle <mtickle@gmail.com>
2022-04-21 11:26:50 -0400

    Update newdash.php

--- newdash.php
+++ newdash.php
@@ -23,12 +23,6 @@
   <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
-  <!-- <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search"> -->
-  <!-- <div class="navbar-nav">
-    <div class="nav-item text-nowrap">
-      <a class="nav-link px-3" href="#">Sign out</a>
-    </div>
-  </div> -->
 </header>
 
 <div class="container-fluid">
@@ -71,16 +65,11 @@
     </nav>
 
 
-    <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4" border="1">
+    <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
       <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
         <h1 class="h2">Dashboard</h1>
-
-
-        
     </div>
 
-
-      </div>
 
       <div class="table-responsive">
         
