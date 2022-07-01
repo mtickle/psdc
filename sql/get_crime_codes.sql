ac0634ae77ee9bfab9d8da7fa8f4c53084dbf4b7
Mike Tickle <mtickle@gmail.com>
2022-07-01 11:15:27 -0400

    zip

---
+++ sql/get_crime_codes.sql
@@ -0,0 +1,35 @@
+-- FUNCTION: public.get_crime_codes()
+
+-- DROP FUNCTION IF EXISTS public.get_crime_codes();
+
+CREATE OR REPLACE FUNCTION public.get_crime_codes(
+	)
+    RETURNS TABLE(crime_code text, crime_description text, incident_count bigint) 
+    LANGUAGE 'plpgsql'
+    COST 100
+    VOLATILE PARALLEL UNSAFE
+    ROWS 1000
+
+AS $BODY$
+begin
+	return query 
+
+SELECT 
+	incident.crime_code
+	, incident.crime_description 
+	, count(incident.crime_code) as incident_count
+FROM
+	public.incident
+GROUP BY
+	incident.crime_code
+	, incident.crime_description 
+ORDER BY
+	incident.crime_code asc;
+
+
+
+	end;
+$BODY$;
+
+ALTER FUNCTION public.get_crime_codes()
+    OWNER TO pi;
