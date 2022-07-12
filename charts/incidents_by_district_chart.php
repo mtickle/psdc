8590cc19db1cd0ceaf9ae28cb7bcb5ea76022f71
Mike Tickle <mtickle@gmail.com>
2022-07-12 11:58:29 -0400

    Update incidents_by_district_chart.php

--- charts/incidents_by_district_chart.php
+++ charts/incidents_by_district_chart.php
@@ -7,11 +7,11 @@
 	//-------------------------------------------------------------
 	//--- Build out the array for the record counts
 	foreach($items as $key => $item) :
-            $temp_description .= "'". $item['district'] . "',";
-           $temp_count .= "'". $item['district_count'] . "',";
-        endforeach;
-        $temp_description = rtrim($temp_description, ',');
-       $temp_count = rtrim($temp_description_count, ',');
+        $temp_description .= "'". $item['district'] . "',";
+        $temp_count .= "'". $item['district_count'] . "',";
+    endforeach;
+    $temp_description = rtrim($temp_description, ',');
+    $temp_count = rtrim($temp_count, ',');
 	//-------------------------------------------------------------
 	
 ?>
@@ -27,7 +27,7 @@ ctxDistricts.height = 350
         data: {
             labels: [<?php echo $temp_description ?>],
             datasets: [{label: 'District',
-                data: [<?php echo$temp_count ?>],
+                data: [<?php echo $temp_count ?>],
                 backgroundColor: ['pink','lightblue','lightgreen','Lavender','AntiqueWhite','Gainsboro'],
                 borderColor: ['black'],
                 borderWidth: 1
