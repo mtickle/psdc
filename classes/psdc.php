68561c5ed234bc6aded8c275bf5c5e81e12e2b70
Mike Tickle <mtickle@gmail.com>
2022-07-12 06:58:33 -0400

    Update psdc.php

--- classes/psdc.php
+++ classes/psdc.php
@@ -1,12 +1,40 @@
 <?php
 
-include("connection.php");
-class Incident {
+
+Class dbConn{
+	/* Database connection start */
+        var $servername, $username, $password, $dbname, $port, $conn;
+
+	function __construct()
+	{
+	  $this->servername = "192.168.86.2";
+	  $this->username = "postgres";
+	  $this->password = "Boomer2025";
+	  $this->dbname = "incident";
+	  $this->port = "5432";
+	}
+	function getConnstring() {
+		$con = pg_connect("host=".$this->servername." port=".$this->port." dbname=".$this->dbname." user=".$this->username." password=".$this->password."") or die("Connection failed: ".pg_last_error());
+
+		/* check connection */
+		//if (pg_last_error()) {
+		//	printf("Connect failed: %s\n", pg_last_error());
+		//	exit();
+		//} else {
+			$this->conn = $con;
+		//}
+		return $this->conn;
+	}
+}
+
+
+
+class Psdc {
 	protected $conn;
 	protected $data = array();
 	public function __construct()  {
 
-		$db = new dbObj();
+		$db = new dbConn();
 		$connString =  $db->getConnstring();
 		$this->conn = $connString;
 	}
