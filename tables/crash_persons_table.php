9544baa22cd81883820111ac5bc31b0ee70dde39
Mike Tickle <mtickle@gmail.com>
2022-07-19 12:06:18 -0400

    Update crash_persons_table.php

--- tables/crash_persons_table.php
+++ tables/crash_persons_table.php
@@ -7,13 +7,19 @@ $items = $objPsdc->getCrashPersons();
     <table id="charge_grid" class="table" width="100%" cellspacing="0">
         <thead>
             <tr>
-                <th>vehicletype</th>
-                <th>contributing_circumstance_1</th>
+                <th>Date of Crash</th>
+                <th>Gender</th>
+                <th>Race</th>
+                <th>Vehicle Type</th>
+                <th>Circumstances</th>
             </tr>
         </thead>
         <tbody>
             <?php foreach($items as $key => $item) :?>
             <tr>
+                <td><?php echo $item['date_of_crash'] ?></td>    
+                <td><?php echo $item['gender'] ?></td>
+                <td><?php echo $item['race'] ?></td>
                 <td><?php echo $item['vehicletype'] ?></td>
                 <td><?php echo $item['contributing_circumstance_1'] ?></td>
             </tr>
